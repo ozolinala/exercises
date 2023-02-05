cfad66b17822365bc307d62d7c63c2fa69cebf99
Laura Ozolina <laur755v@stud.kea.dk>
2023-02-05 20:00:56 +0100

    learnings strings

---
+++ strings.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const longline ="a very, very, very, very, very, VERY \n \
+long line of text, that spans across the width of the editor";
+
+const name ="Laura";
+const animal ="cat";
+const animalname ="Persika";
+
+console.log(`My name is ${name}.\n\
+I have a ${animal} called ${animalname}.`)
